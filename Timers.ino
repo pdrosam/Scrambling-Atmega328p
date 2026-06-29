4d7a7256e0bc7fbbc0c132597e06233c839aadbf
pdrosam <pdrosam25@gmail.com>
2026-06-28 22:32:00 -0400

    multitaskin with millis

--- Timers.ino
+++ Timers.ino
@@ -5,6 +5,10 @@ const int pinBuzzer = 4;
 //  Declarando pines para los dispositivos de entrada
 const int pinButton = 3;
 
+unsigned long previousTime = 0;
+
+const int timeDelay = 1000;
+
 void setup() {
   //Configuramos pines de Entrada y Salida
   pinMode(pinLED, OUTPUT);
@@ -15,11 +19,19 @@ void setup() {
 
 void loop() {
   // Hacemos que un LED parpadee
-  digitalWrite(pinLED, HIGH);
-  delay(1000);
-  digitalWrite(pinLED, LOW);
-  delay(1000);
-
+  unsigned long currentTime = millis();
+  if (currentTime - previousTime >= timeDelay)
+  {
+    previousTime = currentTime;
+    if (digitalRead(pinLED) == HIGH)
+    {
+      digitalWrite(pinLED, LOW);
+    }
+    else
+    {
+      digitalWrite(pinLED, HIGH);
+    }
+  }
   // Presionamos un boton y en funcion de eso sonara un buzzer
   if (digitalRead(pinButton) == LOW) // Preguntamos si el boton esta siendo presionado o no
   {
